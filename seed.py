966c7bf0e43e45cdaf0fa94c24de298e17ed63fb
JosefinaE <josefinaerrazuriz1@gmail.com>
2026-06-29 20:19:59 -0400

    add nota table

--- seed.py
+++ seed.py
@@ -7,6 +7,7 @@ SQL_FILES = [
     "sql_scripts/tarea2.sql",
     "sql_scripts/region-comuna.sql",
     "sql_scripts/tabla-comentario.sql",
+    "sql_scripts/tabla-nota.sql",
     "sql_scripts/seed_data/01_populate_users.sql",
     "sql_scripts/seed_data/02_populate_actividades.sql",
 ]
